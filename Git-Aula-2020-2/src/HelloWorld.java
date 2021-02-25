33dc7eb12f0e5c69636ab045af61ce917d570cc3
victor <victor@VICTOR>
2021-02-25 13:58:13 -0300

    Commit - Victor

--- Git-Aula-2020-2/src/HelloWorld.java
+++ Git-Aula-2020-2/src/HelloWorld.java
@@ -1,14 +1,10 @@
-
 public class HelloWorld {
-
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!!!");
-
 		System.out.println("Copia Eclipse + BASH");
-
-		System.out.println("Merge Eclipse (master + eclipse) e Bash (master e bash)");
-		
+		System.out.println("Merge Eclipse (master + eclipse) e Bash (master e bash)");		
 		System.out.println("Commit - Victor Teixeira Bezerra");
+		
+		System.out.println("Never say never");
 	}
-
 }
