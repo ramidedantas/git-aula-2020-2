cb252a535c3a9296d4ec90d700f6ce812c8d712a
alexsandro-matias <matiasalexsandro@gmail.com>
2021-03-05 11:32:00 -0300

    Alexsandro Matias

--- Git-Aula-2020-2/src/HelloWorld.java
+++ Git-Aula-2020-2/src/HelloWorld.java
@@ -2,14 +2,12 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!!!");
 		System.out.println("Copia Eclipse + BASH");
-		System.out.println("Merge Eclipse (master + eclipse) e Bash (master e bash)");		
+		System.out.println("Merge Eclipse (master + eclipse) e Bash (master e bash)");
 		System.out.println("Commit - Victor Teixeira Bezerra");
 		System.out.println("Commit - Thiago Carrilho Rodriguez");
 		System.out.println("Commit Branch - Thiago Carrilho Rodriguez");
 		System.out.println("Commit Branch - Alexsandro Matias de Almeida");
-		System.out.println("Segundo Commit Branch - Alexsandro Matias de Almeida");
-		
-		
+		System.out.println("Segundo Commit Branch - Alexsandro");
 
 	}
 }
