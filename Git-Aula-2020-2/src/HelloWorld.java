1644d957d1158aade77d604514e281df5ca06e28
Diogo <40742985+Diogomes@users.noreply.github.com>
2021-04-03 10:51:34 -0300

    terceira atividade

--- Git-Aula-2020-2/src/HelloWorld.java
+++ Git-Aula-2020-2/src/HelloWorld.java
@@ -9,6 +9,7 @@ public class HelloWorld {
 		System.out.println("Commit Branch - Alexsandro Matias de Almeida");
 		System.out.println("Segundo Commit Branch - Alexsandro Matias de Almeida");
 		System.out.println("Commit Branch - Diogo do Nascimento Gomes");
+		System.out.println("Segundo Commit Branch - Diogo do Nascimento Gomes");
 		System.out.println("Victor [2]");
 		
 		
